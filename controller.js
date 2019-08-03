6d2507766b1b13065dc4bd7f544de7bca2aaed3d
JMiller-2020 <jgmillr2@illinois.edu>
2019-08-02 20:12:01 -0500

    use .bind(this) for controller events

--- controller.js
+++ controller.js
@@ -11,7 +11,15 @@ class Controller {
   }
 
   _initController() {
-    document.addEventListener('mousedown', () => { this._mouseDown = true })
-    document.addEventListener('mouseup', () => { this._mouseDown = true })
+    document.addEventListener('mousedown', this._handleMouseDown.bind(this))
+    document.addEventListener('mouseup', this._handleMouseUp.bind(this))
+  }
+
+  _handleMouseDown() {
+    this._mouseDown = true
+  }
+
+  _handleMouseUp() {
+    this._mouseDown = false
   }
 }
