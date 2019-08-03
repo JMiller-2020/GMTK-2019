b026c3ef94abf8c5882e75155fe765146e55b4e4
JMiller-2020 <jgmillr2@illinois.edu>
2019-08-03 11:08:48 -0500

    refactor movement and detect world collisions

--- main.js
+++ main.js
@@ -20,26 +20,32 @@ function gameLoop(tickCount) {
   view.clear()
   view.drawWorld(model.world.tiles, model.world.columns)
   view.drawPlayer(
-    model.playerX,
-    model.playerY,
-    model.playerW,
-    model.playerH
+    model.player.x,
+    model.player.y,
+    model.player.w,
+    model.player.h
   )
 }
 
 function tick() {
   const buttonMap = controller.buttonMap
+  let vector = [0, 0]
   if('up' in buttonMap && buttonMap['up']) {
-    model.player.vy -= model.player.acc
+    vector[1] -= 1
   }
   if('down' in buttonMap && buttonMap['down']) {
-    model.player.vy += model.player.acc
+    vector[1] += 1
   }
   if('left' in buttonMap && buttonMap['left']) {
-    model.player.vx -= model.player.acc
+    vector[0] -= 1
   }
   if('right' in buttonMap && buttonMap['right']) {
-    model.player.vx += model.player.acc
+    vector[0] += 1
+  }
+  const unlimitedAcc = Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1])
+  if(unlimitedAcc != 0) {
+    model.player.vx += vector[0] / unlimitedAcc * model.player.acc
+    model.player.vy += vector[1] / unlimitedAcc * model.player.acc
   }
   if('jump' in buttonMap && buttonMap['jump']) {
     // TODO
@@ -61,24 +67,34 @@ function tick() {
   // and jump? Not quite enjoyable yet.
   // model.player.vy += model.gravity
 
-  model.player.X += model.player.vx
-  model.player.Y += model.player.vy
+  model.player.x += model.player.vx
+  model.player.y += model.player.vy
+
+  // world collisions
+  for(let i = 0; i < model.player._collisionPoints.length; i++) {
+    const point = model.player.collisionPoint(i)
+    const loc = point.map(Math.floor)
+    // console.log(model.world.tileAt(...loc))
+    if(model.world.tileAt(...loc).collide) {
+      console.log('collide', loc)
+    }
+  }
 
   // window collisions
-  if(model.player.X < 0) {
-    model.player.X = 0
+  if(model.player.x < 0) {
+    model.player.x = 0
     model.player.vx = Math.max(model.player.vx, 0)
   }
-  if(model.player.X + model.player.W > model.w) {
-    model.player.X = model.w - model.player.W
+  if(model.player.x + model.player.w > model.w) {
+    model.player.x = model.w - model.player.w
     model.player.vx = Math.min(model.player.vx, 0)
   }
-  if(model.player.Y < 0) {
-    model.player.Y = 0
+  if(model.player.y < 0) {
+    model.player.y = 0
     model.player.vy = Math.max(model.player.vy, 0)
   }
-  if(model.player.Y + model.player.H > model.h) {
-    model.player.Y = model.h - model.player.H
+  if(model.player.y + model.player.h > model.h) {
+    model.player.y = model.h - model.player.h
     model.player.vy = Math.min(model.player.vy, 0)
   }
 }
--- model.js
+++ model.js
@@ -6,7 +6,7 @@ class Model {
     this.h = h
 
     this.gravity = 0.4
-    this.maxSpeed = 0.2
+    this.maxSpeed = 0.1
 
     this.player = new Player(2, 2, 0.8, 0.8)
 
@@ -28,19 +28,31 @@ class Player {
     this.vy = 0
     this.acc = 0.02
     this.drag = 0.01
+    this._collisionPoints = this.generateCollisionPoints()
   }
 
-  collisionPoints() {
-    const array = []
-    for(let i = 0; i < this.w; i++) {
-      array.push([this.x + i, this.y         ])  // along the top
-      array.push([this.x + i, this.y + this.h])  // along the bottom
+  collisionPoint(idx) {
+    return [
+      this._collisionPoints[idx][0] + this.x,
+      this._collisionPoints[idx][1] + this.y
+    ]
+  }
+
+  generateCollisionPoints() {
+    const array = [
+      [0, 0],
+      [0, this.h],
+      [this.w, 0]
+    ]
+    for(let i = 1; i < this.w; i++) {
+      array.push([i, 0     ])  // along the top
+      array.push([i, this.h])  // along the bottom
     }
-    for(let j = 0; j < this.h; j++) {
-      array.push([this.x,          this.y + j])  // along the left
-      array.push([this.x + this.w, this.y + j])  // along the right
+    for(let j = 1; j < this.h; j++) {
+      array.push([     0, j])  // along the left
+      array.push([this.w, j])  // along the right
     }
-    array.push([this.x + this.w, this.y + this.h])  // bottom right corner, jic
+    array.push([this.w, this.h])  // bottom right corner, jic
     return array
   }
 }
